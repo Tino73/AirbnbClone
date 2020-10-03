7997622d77c01b8d96344a5e1aea9cccc17692f1
altin thaqi <ltin.thaqi.520@gmail.com>
2020-10-03 14:58:13 +0200

    Implemented Footer

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import Home from './Home'
 import Header from './Header'
+import Footer from './Footer'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
      <Header />
-
      <Home />
+     <Footer />
     </div>
   );
 }
