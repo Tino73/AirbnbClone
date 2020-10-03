4dd36dbc082540f27ddeeff369c8a1fa5b90d7b6
altin thaqi <ltin.thaqi.520@gmail.com>
2020-10-03 13:55:25 +0200

    Implemented Banner

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './Home.css';
+import Banner from './Banner'
 
 
 function Home() {
     return (
         <div classname='home'>
-            
+            <Banner />
+            <div className="home__section">
+                
+            </div>
         </div>
     )
 }
